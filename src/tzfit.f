ffe546bfe685d728489eb712718b1e9c0878a766
David Nice <niced@lafayette.edu>
1998-09-03 13:47:24 +0000

    modified polyco.dat output so psr name and date don't collide

--- src/tzfit.f
+++ src/tzfit.f
@@ -87,7 +87,7 @@ c  log_10 of rms fit error in periods
  
 	write(13,1070) pname(1:10),date,utprint,rfmjd,dmpsr,z,rms,
      +    rphase,f0,nsite,nspan,ncoeff,tzfreq,binpha	
-1070	format(a10,a9,f12.2,f20.11,f21.6,1x,f6.3,f7.3/
+1070	format(a10,1x,a9,f11.2,f20.11,f21.6,1x,f6.3,f7.3/
      +    f20.6,f18.12,3i5,f10.3,a16)
 	write(13,1080) (d(i),i=1,ncoeff)		
 1080	format(3d25.17)
