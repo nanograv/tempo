b03e51023cc916c8e13e81531a07a91ee5516b1d
David Nice <niced@lafayette.edu>
1998-09-18 09:11:28 +0000

    save un-normalized covariance array; clean up code

--- src/fit.f
+++ src/fit.f
@@ -8,7 +8,18 @@ c      $Id$
 	include 'orbit.h'
 	include 'tz.h'
 
-	real*8 xmean(NPA),sigmax(NPA),array(NPA,NPA),fctn(NPAP1)
+c       DJN changes Sep '98
+c       --reformat code (indent loops, end all loops with 'continue')
+c       --get rid of unneeded 'rmul' calculation
+c       --get rid of separate sigmax() calculation since sigmax(i)=array(i,i)
+c       --don't calculate gcor()=array(i,i) just before matinv() since 
+c         array(i,i)=1.0 always here
+c       --add 120/121 loops to un-normalize array() rather than doing so
+c         in sigma0 calculation (this way un-normalized array is eventually
+c         saved in common block, as desired for covar().)
+
+c       moved declaration of real*8 array(NPA,NPA) to acom.h, djn, 8 Sep 98
+	real*8 xmean(NPA),sigmax(NPA),fctn(NPAP1)
 	real*8 r(NPA),a(NPA),sigmaa(NPA),gcor(NPA)
 	logical lw
 	common/dmch/ddmch(NPTSMAX)
@@ -18,49 +29,60 @@ c      $Id$
 	mprt=10**nprnt
 	sigma=0.
 	chisq=0.
-	rmul=0.
-	nterms=nparam-1
-	do 28 j=1,nterms
-	sigmax(j)=0.
-	r(j)=0.
-	a(j)=0.
-	sigmaa(j)=0.
-	do 28 k=1,nterms
-28      array(j,k)=0.
 
-	ymean=ymean/sum
+	nterms=nparam-1
+	do 29 j=1,nterms
+          sigmax(j)=0.
+          r(j)=0.
+          a(j)=0.
+          sigmaa(j)=0.
+          do 28 k=1,nterms
+            array(j,k)=0.
+ 28       continue
+ 29     continue
+
+        ymean=ymean/sum
 	do 53 j=1,nterms
-53	xmean(j)=xmean(j)/sum
+          xmean(j)=xmean(j)/sum
+ 53     continue
 	fnpts=npts-nz
 	wmean=sum/fnpts
 
 	do 67 i=1,npts
-	call vmemr(i,fctn,ct,y,weight,dn,terr,frq,fmjd,rfrq,nterms)
-	weight=weight/wmean
-	sigma=sigma+weight*(y-ymean)**2
-	do 67 j=1,nterms
-	fx=fctn(j)-xmean(j)
-	wfx=weight*fx
-	sigmax(j)=sigmax(j)+wfx*fx
-	r(j)=r(j)+wfx*(y-ymean)
-	do 67 k=1,j
-67	array(j,k)=array(j,k) + wfx * (fctn(k)-xmean(k))
+          call vmemr(i,fctn,ct,y,weight,dn,terr,frq,fmjd,rfrq,nterms)
+          weight=weight/wmean
+          sigma=sigma+weight*(y-ymean)**2
+          do 66 j=1,nterms
+            fx=fctn(j)-xmean(j)
+            wfx=weight*fx
+            sigmax(j)=sigmax(j)+wfx*fx
+            r(j)=r(j)+wfx*(y-ymean)
+            do 65 k=1,j
+              array(j,k)=array(j,k) + wfx * (fctn(k)-xmean(k))
+ 65         continue
+ 66       continue
+ 67     continue
+
+	do 68 i = 1, nterms
+	  sigmax(i) = array(i,i)
+ 68	continue
 
 	free1=fnpts-1
 	sigma=dsqrt(sigma/free1)
 	do 78 j=1,nterms
-	sigmax(j)=dsqrt(sigmax(j)/free1)
-	fs=free1*sigmax(j)
-	r(j)=r(j)/(fs*sigma)
-	do 78 k=1,j
-	array(j,k)=array(j,k)/(fs*sigmax(k))
-78	array(k,j)=array(j,k)
-	do 79 i=1,nterms
-79	gcor(i)=array(i,i)
+          sigmax(j)=dsqrt(sigmax(j)/free1)
+          fs=free1*sigmax(j)
+          r(j)=r(j)/(fs*sigma)
+          do 77 k=1,j
+            array(j,k)=array(j,k)/(fs*sigmax(k))
+            array(k,j)=array(j,k)
+ 77       continue
+ 78     continue
 	call matinv(array,NPA,nterms,det)
 	if(det.eq.0.d0) stop 'fit 78'
 	do 80 i=1,nterms
-80	gcor(i)=sqrt(abs(1.d0 - 1.d0/(gcor(i)*array(i,i))))
+          gcor(i)=sqrt(abs(1.d0 - 1.d0/array(i,i)))
+ 80     continue
 	aa0=ymean
 
 	if(lw) then
@@ -77,73 +99,85 @@ c      $Id$
 	endif
 
 	do 106 j=1,nterms
-	do 104 k=1,nterms
-104	a(j)=a(j)+r(k)*array(j,k)
-	a(j)=a(j)*sigma/sigmax(j)
-106	aa0=aa0-a(j)*xmean(j)
+          do 104 k=1,nterms
+            a(j)=a(j)+r(k)*array(j,k)
+ 104      continue
+          a(j)=a(j)*sigma/sigmax(j)
+          aa0=aa0-a(j)*xmean(j)
+ 106    continue
 
 	do 108 i=1,npts
-	call vmemr(i,fctn,ct,y,weight,dn,terr,frq,fmjd,rfrq,nterms)
-	dt2=y-aa0
-	weight=weight/wmean
-	do 107 j=1,nterms
-107	dt2=dt2-a(j)*fctn(j)
-	nct=ct
-	mark=char(mod(nct,26)+65)
-	date=damoyr(int(fmjd))
-	dt2sec=dt2*p0firs
-	phase=0.
-	if(a1(1).ne.0.)
-     +    phase=dmod((ct-t0(1))*86400.d0/pb(1)+1000000.d0,1.d0)
+          call vmemr(i,fctn,ct,y,weight,dn,terr,frq,fmjd,rfrq,nterms)
+          dt2=y-aa0
+          weight=weight/wmean
+          do 107 j=1,nterms
+            dt2=dt2-a(j)*fctn(j)
+ 107      continue
+          nct=ct
+          mark=char(mod(nct,26)+65)
+          date=damoyr(int(fmjd))
+          dt2sec=dt2*p0firs
+          phase=0.
+          if(a1(1).ne.0.)
+     +         phase=dmod((ct-t0(1))*86400.d0/pb(1)+1000000.d0,1.d0)
 
-	if(nprnt.eq.0.or.mod(i,mprt).eq.1) then
-	write(adn,1106) dn
-1106	format(f14.0)
+          if(nprnt.eq.0.or.mod(i,mprt).eq.1) then
+            write(adn,1106) dn
+ 1106       format(f14.0)
 
-	if(lw) write(31,1107) i,fmjd,mark,date,
-     +    rfrq,weight,adn(1:13),y,dt2sec/unit,phase
-1107    format(i4,f12.5,1x,a1,1x,a9,f9.3,f6.3,a13,
-     +    f8.3,f8.2,f7.3)
-	endif
+            if(lw) write(31,1107) i,fmjd,mark,date,
+     +           rfrq,weight,adn(1:13),y,dt2sec/unit,phase
+ 1107       format(i4,f12.5,1x,a1,1x,a9,f9.3,f6.3,a13,
+     +           f8.3,f8.2,f7.3)
+          endif
 
 C Correct tz ref TOA
-	if(i.eq.ntzref) tzrmjd=tzrmjd-dt2sec/8.64d4
+          if(i.eq.ntzref) tzrmjd=tzrmjd-dt2sec/8.64d4
 
-	if((i.lt.npts.or.(.not.gro)).and.lw) write(32) ct,dt2,
-     +    dt2sec,phase,frq,weight,terr,y,ddmch(i)
-	wmax=max(wmax,weight)
-108	chisq=chisq+weight*dt2**2
+          if((i.lt.npts.or.(.not.gro)).and.lw) write(32) ct,dt2,
+     +         dt2sec,phase,frq,weight,terr,y,ddmch(i)
+          wmax=max(wmax,weight)
+          chisq=chisq+weight*dt2**2
+ 108    continue
 
 	freen=fnpts-nterms-1
 	chisqr=chisq*wmean/freen
 	if(mode.eq.0) varnce=chisqr
 	if(mode.ge.1) varnce=1./wmean
 	varfit=chisq/fnpts
-	if(mode.eq.1) varfit=chisq/fnpts
 	if(mode.eq.0) chisqr=0.
+
+	do 120 j = 1, nterms
+	  do 121 k = 1, nterms
+	    array(j,k) = array(j,k)*varnce/(free1*sigmax(j)*sigmax(k))
+ 121	  continue
+ 120	continue
+
 	do 133 j=1,nterms
-	sigmaa(j)=dsqrt(array(j,j)*varnce/free1)/sigmax(j)
-133	rmul=rmul+a(j)*r(j)*sigmax(j)/sigma
-C	freej=nterms
-C	ftest=(rmul/freej)/((1.-rmul)/freen)
-	rmul=dsqrt(rmul)
+          sigmaa(j)=dsqrt(array(j,j))
+ 133    continue
 	sigma0=varnce/fnpts
 	do 145 j=1,nterms
-	do 145 k=1,nterms
-145	sigma0=sigma0+varnce*xmean(j)*xmean(k)*array(j,k) /
-     +  (free1*sigmax(j)*sigmax(k))
+          do 144 k=1,nterms
+            sigma0=sigma0+xmean(j)*xmean(k)*array(j,k) 
+ 144      continue
+ 145    continue
+
 	sigma0=dsqrt(sigma0)
 
 	do 146 j=1,NPAP1
-	freq(j)=0.
-146	ferr(j)=0.
+          freq(j)=0.
+          ferr(j)=0.
+ 146      continue
 
 	freq(1)=aa0
 	ferr(1)=sigma0
 
 	do 150 j=1,nterms
-	freq(mfit(j+1))=a(j)
-150	ferr(mfit(j+1))=sigmaa(j)
+          freq(mfit(j+1))=a(j)
+          ferr(mfit(j+1))=sigmaa(j)
+ 150    continue
+
 
 	return
 	end
