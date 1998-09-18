59fd54c506a461a5f46002b637373d86cb216f0b
David Nice <niced@lafayette.edu>
1998-09-18 09:11:00 +0000

    added covariance matrix array() to common block

--- src/acom.h
+++ src/acom.h
@@ -1,8 +1,9 @@
 c      $Id$
+
 	logical gro,sim,xitoa,oldpar,psrframe,eclcoord
 	logical usestart, usefinish
 	integer parunit, nskip, iboot
-	character psrname*12,obsflag*1,pardir*80,obskey*5
+
 	common pdec,pra,ba(3),bc(3),dm,dt,dt2,freq(NPAP1),
      +    ferr(NPAP1),fmin,hlt(36),hrd(36),wt,x(NPAP1),
      +    era,ec,erd,fmax,tmax,start,finish,amjd1,amjd2,posepoch,posep,
@@ -11,4 +12,13 @@ c      $Id$
      +    nkeep,nfq,ncoord,gro,sim,xitoa,oldpar,psrframe,
      +	  eclcoord,usestart,usefinish,parunit,nskip,iboot,ndmcalc,
      +    nfcalc
+
+
+	character psrname*12,obsflag*1,pardir*80,obskey*5
+
         common/acomch/psrname,pardir,obsflag,obskey(36) 
+
+
+        real*8 array(NPA,NPA) ! moved here from fit.f, djn, 8-Sep-98
+
+        common/acomcov/array
