ee8e5728ce5db8636cd1e30fdd2995178a3c50a4
David Nice <niced@lafayette.edu>
2000-05-03 01:19:22 +0000

    Changes to make epoch in polyco.dat more accurate: [1] always round epoch to nearest 0.000 000 000 1 day, so that no     digits are lost in polyco.dat output [2] separate integeger and fractional part of MJDs throughout polyco     calculations

--- src/atimfake.f
+++ src/atimfake.f
@@ -24,7 +24,7 @@ C	@(#)atimfake.f	9.19 2/3/94
  1013	   format(15x,f9.0)
 	else
 	   i=0                                         ! Write ref toa line
-	   write(50,1040)tzrsite,i,pname(1:8),tzrfrq,tzrmjd
+	   write(50,1041)tzrsite,i,pname(1:8),tzrfrq,tzrmjd
 	endif
 
 	nspan=nsp(ipsr)
@@ -39,8 +39,10 @@ C	@(#)atimfake.f	9.19 2/3/94
 	rax=irah+(iram+2)/60.			
 	wait=(rax-hlst)*0.99727				!Solar to sidereal
 	if(wait.lt.-maxha) wait=wait + 23.9345
-	fmjd1=afmjd+(wait-maxha)/24.+nspan/2880.		!Compute start time
-	fmjd1=nint(48*fmjd1)/48.d0	
+	fmjd1=afmjd+(wait-maxha)/24.+nspan/2880.	!Compute start time
+        nmjd1 = int(fmjd1)      ! break into integer, fractional parts
+        fmjd1 = fmjd1 - nmjd1
+	fmjd1=nint(48*fmjd1)/48.d0 ! round to nearest half-hour?!
 	nsets=(120*maxha+nspan-1)/nspan
  
 	b=nspan/2 + 5
@@ -48,21 +50,40 @@ C	@(#)atimfake.f	9.19 2/3/94
 	bma=0.5*(b-a)
 	bpa=0.5*(b+a)
 	do 30 k=1,n
-30	x(k)=cos(pi*(k-0.5)/n)*bma+bpa
+          x(k)=cos(pi*(k-0.5)/n)*bma+bpa
+ 30     continue
  
 	i=0
 	do 50 j=1,nsets				
-	fmjd2=fmjd1+(j-1)*nspan/1440.d0
-	do 40 k=1,n
-	i=i+1
-	if(i.gt.800) stop ' Nspan too small'
-	tfmjd(i)=fmjd2+x(k)/1440.d0
-	tmin(i)=1440.d0*(tfmjd(i)-tfmjd(1))
-	write(50,1040) tzsite,i,pname(1:8),tzfreq,tfmjd(i)
- 40	continue
+          fmjd2 = fmjd1 + (j-1)*nspan/1440.d0
+          ntmp1 = int(fmjd2*1.d8) ! round fmjd2 to 1.e-10 (messy since ints
+          ntmp2 = int((fmjd2 *1.d8-ntmp1)*1.d2) ! don't have 10 significant
+          fmjd2 = ntmp2*1.d-10 + ntmp1*1.d-8 !    digits)
+          do 40 k=1,n
+            i = i + 1
+            if(i.gt.800) stop ' Nspan too small'
+            ftmjd(i) = fmjd2 + x(k)/1440.d0
+            ntmjd(i) = nmjd1
+            if (ftmjd(i).ge.2.) then
+              ftmjd(i) = ftmjd(i) - 2.
+              ntmjd(i) = ntmjd(i) + 2
+            elseif (ftmjd(i).ge.1.) then
+              ftmjd(i) = ftmjd(i) - 1.
+              ntmjd(i) = ntmjd(i) + 1
+            elseif (ftmjd(i).lt.0.) then
+              ftmjd(i) = ftmjd(i) + 1.
+              ntmjd(i) = ntmjd(i) - 1
+            endif
+            tmin(i) = 1440.d0*((ntmjd(i)-ntmjd(1))+ftmjd(i)-ftmjd(1))
+            nx = ntmjd(i)/10
+            fx = (ntmjd(i)-10*nx)+ftmjd(i)
+            write(50,1040) tzsite,i,pname(1:8),tzfreq,nx,fx
+ 40       continue
  50	continue
-1040	format(a1,i5,1x,a,f9.3,f20.13)
- 
+ 1040   format(a1,i5,1x,a,f9.3,i4,f16.14)
+ 1041   format(a1,i5,1x,a,f9.3,f20.13)
+
+
 	nt=i
 	return
 	end
