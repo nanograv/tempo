f77e6820bca3231980137a41b1d7154d8a271d99
David Nice <niced@lafayette.edu>
2004-02-18 21:16:22 +0000

    new routines to convert between numerical and ASCII site names

---
+++ src/sitea2n.f
@@ -0,0 +1,22 @@
+      integer function sitea2n(s)
+
+C     convert first character of ascii string s into a numerical
+C     observatory code
+
+      implicit none
+
+      character*(*) s
+
+      if(s(1:1).ge.'0'.and.s(1:1).le.'9')then
+        sitea2n=ichar(s(1:1))-48
+      else if(s(1:1).ge.'a'.and.s(1:1).le.'z')then
+        sitea2n = ichar(s(1:1))-87
+      else if (s(1:1).eq.'@') then
+        sitea2n = -1
+      else
+        sitea2n = -2
+      endif
+      
+      return
+      
+      end
---
+++ src/siten2a.f
@@ -0,0 +1,19 @@
+      character*1 function siten2a(n)
+
+C     convert integer n into an ASCII observatory code
+
+
+      implicit none
+
+      integer n
+
+      if (n.eq.-1) then
+        siten2a = '@'
+      else if (n.ge.0.and.n.le.9) then
+        siten2a = char(48+n)
+      else if (n.ge.10.and.n.le.35) then
+        siten2a = char(55+n)
+      endif
+      return
+
+      end
