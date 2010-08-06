f788f5f1e46134cc06fd4badf1ac7be03ac5d81b
Paul Demorest <pdemores@nrao.edu>
2010-08-06 19:44:24 +0000

    Fix remaining zero-weight/iteration bug.

--- src/arrtim.f
+++ src/arrtim.f
@@ -378,8 +378,10 @@ c   Back to processing of all TOAs
           end do
           if (nonewdmx) then ! give this point zero weight, don't create new range
             wgt = 0
-	    if (jits.eq.0) nz = nz + 1
-            zerowt(nz) = n
+            if (jits.eq.0) then 
+              nz = nz + 1
+              zerowt(nz) = n
+            endif
             idmx = 0
             goto 85
           endif
