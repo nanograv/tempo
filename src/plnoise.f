76a3bbc77f1553dbf8c3a5d3ab358888e887c8ef
Paul Demorest <pdemores@nrao.edu>
2014-04-15 17:44:45 +0000

    plnoise: change units for amp, idx to "timing" (not "GW")

--- src/plnoise.f
+++ src/plnoise.f
@@ -23,22 +23,32 @@ c years, output in in us^2.
         real*8 scale0, scale1
         real*8 log_coeffs(NC), signs(NC)
         real*8 ctmp,xtmp,lxtmp,ssum
-
-c TODO replace alpha with "timing" spec idx?
+        real*8 exval
 
         save pi, fl, scale0, scale1, signs, log_coeffs
 
+c exval is the value that appears in many exponents in the
+c formula.  If input alpha is the strain spectral index, then:
+        !exval = 2.0-2.0*alpha
+c OR, if alpha is instead the timing power index, then:
+        exval = -1.0-alpha
+
+c This output scaling assumes input amp is in strain units:
+c          scale0 = f0**(exval-2.0)/(12.0*pi**2)/(fl**(exval))
+c          scale0 = scale0 * (365.24*86400.0)**2 * 1d12 ! us^2 output
+c If instead amp is in "observer units" of us / sqrt(yr^-1) then:
+          scale0 = f0 * (f0/fl)**exval
+
+c First-time initialization, only depends on spec idx
         if (init) then
-          scale0 = f0**(-2.0*alpha)/(4.0*pi**2)/(fl**(2.0-2.0*alpha))
-          scale0 = scale0 * (365.24*86400.0)**2 * 1d12 ! us^2 output
-          scale1 = gamma(-2.0+2.0*alpha) * cos(pi*alpha)
+          scale1 = gamma(-exval) * cos(pi*(1.0-exval/2.0))
           do i=0,NC-1
             if (mod(i,2).eq.1) then
               signs(i+1) = -1.0
             else
               signs(i+1) = 1.0
             endif
-            ctmp = 2.0d0*i + 2.0*alpha - 2.0
+            ctmp = 2.0d0*i - exval
             if (ctmp.lt.0.0) then
               signs(i+1) = -1.0 * signs(i+1)
               ctmp = abs(ctmp)
@@ -59,7 +69,7 @@ c TODO replace alpha with "timing" spec idx?
         do i=0,NC-1
           ssum=ssum+signs(i+1)*exp(2.0d0*i*lxtmp + log_coeffs(i+1))
         enddo
-        plnoise = (amp**2)*scale0*(-scale1*(xtmp**(2.0-2.0*alpha))-ssum)
+        plnoise = (amp**2)*scale0*(-scale1*(xtmp**(exval))-ssum)
 
         return
         end
@@ -83,12 +93,13 @@ c ===================================================================
 
         if (init) then
           lu(1) = plnoise(0d0,alpha,f0,amp,.true.)
+c          print *,"PL0 =", lu(1)
           do i=1,NPTS
             lu(i+1) = plnoise(i/365.24d0,alpha,f0,amp,.false.)
           enddo
         endif
 
-c Assume dt in days.. should really make these all consistent...
+c Assume dt is in days.. should really make these all consistent...
         idx0 = floor(abs(dt))
         frac = abs(dt) - idx0
         plnoise_interp = (1d0 - frac)*lu(idx0+1) + frac*lu(idx0+2)
